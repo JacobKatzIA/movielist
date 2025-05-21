6c8f8ebe08ba2e4598d65f8c086c0cd443d4c0e9
JacobKatzIA <jacke.katz@gmail.com>
2025-05-21 15:43:25 +0200

    Skapat ytterligare komponenter såsom movie.js, movielist.js och fixat funktionalitet för att lägga till samt radera objekt som läggs till i listan

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,10 +1,38 @@
 // Denna ska hantera min filmlista
 import React, { useState, useRef} from 'react';
+import Movie from './Movie';
 
 export default function MovieList() {
+    const [movies, setMovies] = useState([{
+        id: 1,
+        title: "First Movie"
+    }]);
+
+    const inputRef = useRef()
+
+    function addItem(event) {
+        if (event.keyCode === 13) {
+            const newID = movies.length > 0 ? movies[movies.length - 1].id + 1 : 1;
+
+            setMovies([...movies, {
+                id: newID,
+                title: inputRef.current.value
+            }]);
+
+            inputRef.current.value = "";
+        }
+    }
+
+    function deleteItem(id) {
+        setMovies(movies.filter((item) => item.id !== id));
+    }
+
      return (
         <div>
-            <input className="form-control" placeholder='Add New Movie here...' ></input>
+            <input className="form-control" placeholder="Add New Movie here..." ref={inputRef} onKeyUp={addItem} ></input>
+            <ul className="list-group">
+                { movies.map(movie => <Movie key={movie.id} item={movie} deleteItem={deleteItem} />) }
+            </ul>
         </div>
      )
 }

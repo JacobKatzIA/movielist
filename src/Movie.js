d091ff9a05c65047bce0bc0ff8b9af2fede97c51
JacobKatzIA <jacke.katz@gmail.com>
2025-05-21 20:50:57 +0200

    Lagt till en for-loop som styr hur många stjärnbilder som ska visas vid sidan om varje filmtitel beroende av vilket betyg som användaren klickar i 1-5. Skapat en css-fil som ser säkerställer att stjärnorna har en bra layout samt att de ligger ute i högra hörnet till vänster om delete-ikonen

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import './Movie.css';
 
 export default function Movie(props) {
+    const {title, rating, id } = props.item
+
+    const stars = [];
+    for (let i = 0; i < rating; i++) {
+        stars.push(
+            <img
+            key={i}
+            src="/images/star.png"
+            alt="Star image"
+            style={{height: "20px", marginLeft: "2px"}}
+            />
+        )
+    } 
+
+
     return (
-        <li className="list-group-item">
-            { props.item.title}
+        <li className="list-group-item movie-item">
+            {title} 
             <button className="btn btn-sm btn btn-danger float-end" onClick={() => {props.deleteItem(props.item.id)}}>X</button>
+            <span className="movie-stars">{stars}</span>
         </li>
     )
 }

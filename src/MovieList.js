0be13e97476ee657f645a511b90855b551d61210
JacobKatzIA <jacke.katz@gmail.com>
2025-05-21 18:27:06 +0200

    Implementerat funktionalitet från min inl3a så att jag nu kan lägga till filmers titel med ett betyg. Jag har gjort mig av med keycode13 och istället lagt till en knapp som tillåter mig att lägga till filmer i min lista

--- src/MovieList.js
+++ src/MovieList.js
@@ -5,23 +5,38 @@ import Movie from './Movie';
 export default function MovieList() {
     const [movies, setMovies] = useState([{
         id: 1,
-        title: "First Movie"
+        title: "First Movie",
+        rating: 5
     }]);
 
-    const inputRef = useRef()
+    const inputRef = useRef();
+    const ratingRef = useRef();
 
-    function addItem(event) {
-        if (event.keyCode === 13) {
+    function addItem() {
+            const title = inputRef.current.value;
+            const rating = parseInt(ratingRef.current.value);
+
+            if (title === "") {
+                alert("Du måste ange en titel");
+                return;
+            }
+
+            if (rating === 0) {
+                alert("Du måste ange ett betyg");
+                return;
+            }
+
             const newID = movies.length > 0 ? movies[movies.length - 1].id + 1 : 1;
 
             setMovies([...movies, {
                 id: newID,
-                title: inputRef.current.value
+                title: title,
+                rating: rating
             }]);
 
             inputRef.current.value = "";
+            ratingRef.current.value = "0";
         }
-    }
 
     function deleteItem(id) {
         setMovies(movies.filter((item) => item.id !== id));
@@ -29,7 +44,19 @@ export default function MovieList() {
 
      return (
         <div>
-            <input className="form-control" placeholder="Add New Movie here..." ref={inputRef} onKeyUp={addItem} ></input>
+            <input className="form-control" placeholder="Add New Movie here..." ref={inputRef}></input>
+
+            <select className="form-control mb-3" ref={ratingRef}>
+                <option value="0">Välj betyg...</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+
+            <button className="btn btn success mb-3" onClick={addItem}>Spara film</button>
+
             <ul className="list-group">
                 { movies.map(movie => <Movie key={movie.id} item={movie} deleteItem={deleteItem} />) }
             </ul>

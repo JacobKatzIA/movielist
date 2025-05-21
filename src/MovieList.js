3d9efda53119229c456a09e5074a2fa3a789f17b
JacobKatzIA <jacke.katz@gmail.com>
2025-05-21 14:53:05 +0200

    Lagt till movielist komponent

---
+++ src/MovieList.js
@@ -0,0 +1,10 @@
+// Denna ska hantera min filmlista
+import React, { useState, useRef} from 'react';
+
+export default function MovieList() {
+     return (
+        <div>
+            <input className="form-control" placeholder='Add New Movie here...' ></input>
+        </div>
+     )
+}
